b7dc6e69984d9d28baab173c1843262d783588dd
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 15:22:58 +0500

    Add files via upload

---
+++ maim.ts
@@ -0,0 +1,5 @@
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.Make sure you use each character combination, "\t" and "\n", at least once.Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let Namewhitwhitespace ="\n\t Asadullah \t\n";
+console.log(Namewhitwhitespace);
+let Namewithoutwhitespace = Namewhitwhitespace .trim();
+console.log( Namewithoutwhitespace );
